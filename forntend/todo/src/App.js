e9518c5d98325ab67649109fad16fe711fa32684
Neema <patelneema93@gmail.com>
2026-06-04 11:34:55 -0400

    app.css change to app.scss and index.css to change index.scss

--- forntend/todo/src/App.js
+++ forntend/todo/src/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+       <div className="container">
+      <h1>Todo App</h1>
     </div>
   );
 }
