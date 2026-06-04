8f1be5cfc5a3965724f0f72e104e26c44a8d03ab
Neema <patelneema93@gmail.com>
2026-06-04 15:28:59 -0400

    create TodoItem.scss and app.js file update

--- forntend/todo/src/App.js
+++ forntend/todo/src/App.js
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import './App.scss';
-import Header from './component/Header';
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    const res = await axios.get("http://localhost:8000/tasks");
+    setTasks(res.data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:8000/task/${id}`);
+    fetchTasks();
+  };
+
+  const handleToggle = async (task) => {
+    await axios.put(`http://localhost:8000/task/${task.id}`, {
+      name: task.name,
+      checked: !task.checked,
+    });
+
+    fetchTasks();
+  };
+
   return (
-       <div className="container">
-   <Header onAdd={fetchTasks} />
+    <div className="app">
+      <Header onAdd={fetchTasks} />
+
+      <TodoList
+        tasks={tasks}
+        onDelete={handleDelete}
+        onToggle={handleToggle}
+      />
     </div>
   );
 }
