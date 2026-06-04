65f9615e90a730a99901ef705aed0aa81b13a223
Neema <patelneema93@gmail.com>
2026-06-04 14:14:35 -0400

    Todoinput scss create

--- forntend/todo/src/component/Header.jsx
+++ forntend/todo/src/component/Header.jsx
@@ -1,31 +1,13 @@
 import { useState } from "react";
 import "./Header.scss";
+import Todoinput from "./Todoinput";
 function Header() {
-  const [todo, setTodo] = useState("");
-
-  const handleAdd = () => {
-    if (!todo.trim()) return;
-
-    console.log("New Todo:", todo);
-    setTodo("");
-  };
+  
 
   return (
     <header className="header">
       <h1>Todo App</h1>
-
-      <div className="todo-input">
-        <input
-          type="text"
-          placeholder="Enter a task..."
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-
-        <button onClick={handleAdd}>
-          Add
-        </button>
-      </div>
+    <Todoinput />
     </header>
   );
 }
---
+++ forntend/todo/src/component/Todoinput.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./Header.scss";
+export default function Todoinput() {
+const [todo, setTodo] = useState("");
+
+  const handleAdd = () => {
+    if (!todo.trim()) return;
+
+    console.log("New Todo:", todo);
+    setTodo("");
+  };
+  return (
+   
+
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+
+        <button onClick={handleAdd}>
+          Add
+        </button>
+      </div>
+   
+  );
+
+}
